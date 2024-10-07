2ab6a45249aa292eb7d4e6785cc16bdee764a633
Nigel Koremans <155839264+HyperNeutron@users.noreply.github.com>
2024-10-07 10:11:11 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer class="wrapper">
+	<p>&copy; 2020 Naam en Naam - Groep XX - studenten bij Curio.</p>
+	<p class="muted">Deze website is gebouwd als schoolopdracht voor het eerste leerjaar van de opleiding <em>Software Developer</em></p>
+</footer>
---
+++ header.php
@@ -0,0 +1,10 @@
+<header>
+	<div class="wrapper">
+		<nav>
+			<a href="index.php"><img src="http://picsum.photos/100" alt="logo" class="logo"></a>
+			<a href="index.php">Home</a>
+			<a href="#">(pagina)</a>
+			<a href="contact.php">Contact</a>
+		</nav>
+	</div>
+</header>
