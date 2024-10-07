3db806a98f1dea81fe2a9231d68ba0a77fab7da1
Nigel Koremans <155839264+HyperNeutron@users.noreply.github.com>
2024-10-07 10:13:21 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <footer class="wrapper">
-	<p>&copy; 2020 Naam en Naam - Groep XX - studenten bij Curio.</p>
+	<p>&copy; 2024 Nazar, Arsenii, Nigel en Boy - Groep TTSDR-SD4O24A - studenten bij Curio.</p>
 	<p class="muted">Deze website is gebouwd als schoolopdracht voor het eerste leerjaar van de opleiding <em>Software Developer</em></p>
 </footer>
