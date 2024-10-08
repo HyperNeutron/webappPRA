d48938b3b26c25ec62df458c62fda3ce047d79ba
emxnator <nazar251107@gmail.com>
2024-10-08 11:44:12 +0200

    Add files via upload

---
+++ team.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Contact</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<?php require_once("header.php"); ?>
+
+    <main>
+       <div class="wrapper">
+         <img src="img/team.jpg"> 
+         <style>
+            main img{
+	            width: 900px;
+                height: 550px;
+             }
+         </style>
+         <h2>Onze team</h2>
+         <p>Wat houdt het 30+ voetbal in? De 7x7 30+ voetbal voor vrouwen: Steeds meer verenigingen zien de voordelen om te starten met deze voetbalvorm voor vrouwen. Niet vreemd, want het is ontstaan vanuit de behoefte van veel speelsters die graag een balletje trappen, maar om verschillende redenen dit niet (meer) willen of kunnen in de reguliere competitie. Speelsters met een drukke agenda die niet meer wekelijks willen spelen, oud-speelsters die nog graag een potje voetballen op een recreatieve manier, teams die door een terugloop van het aantal leden geen elftal meer op de been kunnen brengen. Ook voor startende teams die nog onvoldoende speelsters hebben om een compleet elftal samen te stellen, biedt 7x7 voetbal extra mogelijkheden. Juist voor deze groepen is het 7x7 30+ voetbal ontwikkeld. Deze voetbalvorm blijkt een mooie aanvulling te zijn, zo hebben verenigingen voor ieder wat wils. Wij hebben een heel gezellig team van moeders die bijna allemaal nog nooit gevoetbald hebben. We zijn niet allemaal heel fanatiek en we winnen bijna nooit, maar we hebben altijd wel veel plezier!</p>
+    <?php require_once("footer.php"); ?>
+</body>
---
+++ trainingen.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Contact</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<?php require_once("header.php"); ?>
+
+    <main>
+       <div class="wrapper">
+         <img src="img/football.jpg">
+         <h2>Hoe we de treinen hebben gedaan</h2>
+    <p>Wij trainen op:</p>
+    <ul>
+        <li>Maandag van 19.30 tot 21.00 uur.</li>
+        <li>Woensdag van 19.30 tot 21.00 uur.</li>
+    </ul>
+    <p>Oefeningen die we trainen:</p>
+    <ul>
+        <li>1 vs 1 transitie
+Oefen je 1 op 1 skills op zowel verdedigend als aanvallend gebied. Deze snelle 1 vs 1 training kan je inzetten om de skills te oefenen, de balcontrole maar zeker ook de conditie van je spelers.
+</li>
+<li>Bal afschermen door lichaam te gebruiken
+Het vervolg op de eerste oefening over zoneverdediging is deze spelvariant. Na een grondige uitleg aan de verdedigers stel je zowel de aanval als de verdediging bloot aan dit effectieve spelprincipe
+</li>
+<li>Zoneverdediging in wedstrijdvorm
+Het vervolg op de eerste oefening over zoneverdediging is deze spelvariant. Na een grondige uitleg aan de verdedigers stel je zowel de aanval als de verdediging bloot aan dit effectieve spelprincipe.
+</li>
+    </ul>
+
+    <?php require_once("footer.php"); ?>
+</body>
